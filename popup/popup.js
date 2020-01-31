567aa68a669b0f233a5b85f0fad93e3ecf1f0bc6
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-31 17:39:11 +0100

    Add codes

---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+chrome.tabs.query({active: true, currentWindow: true}, (tabs)=> {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "get_images"}, response => {
+        $('.gallery').html('');
+        response.map((img) => {
+            $('.gallery').append('<img src="' + Image.src + '" />');
+        })
+    })
+})
