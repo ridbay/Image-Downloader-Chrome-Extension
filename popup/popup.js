c13053d289b3c66b78d8be365d1da9d7b9334ccd
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-31 17:45:23 +0100

    Add codes in popup.js to show all images on a page

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,9 @@
-chrome.tabs.query({active: true, currentWindow: true}, (tabs)=> {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "get_images"}, response => {
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "get_images" }, response => {
         $('.gallery').html('');
         response.map((img) => {
             $('.gallery').append('<img src="' + img.src + '" />');
-           
+
         })
     })
 })
